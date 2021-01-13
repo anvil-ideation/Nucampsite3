7820f64a667f7c73d726e051d01c99a34a37ba25
anvil-ideation <landon@anvilideation.com>
2021-01-13 16:33:11 -0700

    Animatable - revised

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -19,7 +19,7 @@ function Mission () {
             title='Our Mission'
         >
             <Text>
-            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
     )
@@ -43,6 +43,16 @@ class About extends Component {
             );
         };
 
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Loading />
+                    </Animatable.View>
+                </ScrollView>
+            );
+        }
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
@@ -55,34 +65,20 @@ class About extends Component {
                 </ScrollView>
             );
         }
-        if(this.props.partners.errMess) {
-            return (
-                <ScrollView>
-                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
-                        <Mission />
-                        <Card title="Community Partners">
-                            <FlatList
-                                data={this.props.partners.partners}
-                                renderItem={renderPartner}
-                                keyExtractor={item=>item.id.toString()}
-                            />
-                        </Card>
-                    </Animatable.View>
-                </ScrollView>
-            );
-        }
         return (
             <ScrollView>
-                <Mission />
-                <Card
-                    title='Community Partners' 
-                >
-                    <FlatList
-                        data={this.props.partners.partners}
-                        renderItem={renderPartner}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Mission />
+                    <Card
+                        title='Community Partners' 
+                    >
+                        <FlatList
+                            data={this.props.partners.partners}
+                            renderItem={renderPartner}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
